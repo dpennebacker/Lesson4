0e4d756a69e02e5f037730d750a3c3cc0c27a515
Daniel Pennebacker <dpennebacker@Daniels-MacBook-Pro-4.local>
2016-09-14 12:50:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int i = 3;
+        //this is a nogo: 3 = i;
+        double p = 234.3;
+        double j;
+        //can I say: j = p;
+        j=p;
+        System.out.println(j);
+        //Do you remember...
+        //and please excuse her
+        //my dear aunt sally?
+        //PEMDAS
+        //P = parenthesis? ()
+        //E = exponent ^
+        //m = multiply *
+        //D = divide /
+        //A add +
+        //s subtract -
+        //modulus %
+
+        int count = 0;
+        count = count + 3;
+
+        //increment operator
+        count++;
+        count = count + 1;
+        //equal
+        count--;
+        ++count;
+
+        count+=3;
+        count = count + 3;
+        count-=3;
+        count = count - 3;
+        count/=3;
+        count = count /3;
+        count*=3;
+        count = count * 3;
+
+
+
+
+
+        int drAux = 3;
+        int paul = 2 + drAux++;
+        //SOP(paul)
+
+        int x = 5;
+        int y = 2;
+        System.out.println(x/y);
+
+        //Lesson 5 is below
+        double p = 29.78;
+        int ii = (int)p;
+
+
+        final double taxRate = .06;
+        //Why would this be smart?
+    }
+}
